ee748759d374b06cfba1aced9cc799752b28dd4d
The-indigo <adepojuadeyemi11@gmail.com>
2022-09-13 13:09:19 -0400

    mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useEffect } from 'react';
 import './App.css';
 import Home from './components/Intro/Home';
 import Nav from './components/Nav/Nav';
 
 const App = () => {
   
+  // useEffect(() => {
+  //   var docWidth = document.documentElement.offsetWidth;
+  //   console.log(docWidth);
+
+  //   [].forEach.call(
+  //     document.querySelectorAll('*'),
+  //     function (el) {
+  //       if (el.offsetWidth > docWidth) {
+          
+  //         console.log(el)
+  //       }
+  //     }
+  //   )
+  // })
 
   return (
     <div >
